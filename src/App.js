52a8233e10955a6242b595df0060a436cc65f314
yuyasat <takiyour@gmail.com>
2023-01-19 14:22:07 +0900

    getPokemon

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon } from './utils/pokemon.js';
+import { getAllPokemon, getPokemon } from './utils/pokemon.js';
 
 function App() {
   const initialUrl = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialUrl);
-      console.log(res);
+
+      loadPokemon(res.results);
+      setLoading(false);
     }
     fetchPokemonData();
   }, [])
+  const loadPokemon = async (data) => {
+    // Promise.allの中には配列を入れる。すべてfetchが終わるまで待つ
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    )
+    setPokemonData(_pokemonData);
+  };
+  console.log(pokemonData)
   return (
     <div className="App">
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <h1>ポケモンデータを取得しました</h1>
+      )}
     </div>
   );
 }

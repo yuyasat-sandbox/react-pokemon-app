c3b0036bacf00fb4e4584fd73365db5a845b4506
yuyasat <takiyour@gmail.com>
2023-01-19 13:43:29 +0900

    getAllPokemon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from './utils/pokemon.js';
 
 function App() {
+  const initialUrl = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialUrl);
+      console.log(res);
+    }
+    fetchPokemonData();
+  }, [])
   return (
     <div className="App">
     </div>
